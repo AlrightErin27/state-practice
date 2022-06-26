f1ae19767b8e2d12c7cb067cf82cde9b3db88633
Erin Van Brunt <erinmontybruce@gmail.com>
2022-06-26 15:31:03 -0700

    updated

--- src/components/RandomCard.js
+++ src/components/RandomCard.js
@@ -23,6 +23,12 @@ function RandomCard(props) {
       "🍄",
       "🍩",
       "⚽️",
+      "🪲",
+      "⭐️",
+      "🦞",
+      "🦜",
+      "🍓",
+      "⏰",
     ];
     let randomCard = cardsArr[Math.floor(Math.random() * cardsArr.length)];
     setCardStatus((cardStatus) => randomCard);
@@ -37,12 +43,14 @@ function RandomCard(props) {
       <div className="RandomCard">
         <h1 id="image">{cardStatus}</h1>
       </div>
-      <button class="button" onClick={handleCardStatus}>
-        {props.buttonTxt}
-      </button>
-      <button class="button" onClick={handleReset}>
-        Reset
-      </button>
+      <div className="button-cont">
+        <button class="button" onClick={handleCardStatus}>
+          {props.buttonTxt}
+        </button>
+        <button class="button" onClick={handleReset}>
+          Reset
+        </button>
+      </div>
     </>
   );
 }
